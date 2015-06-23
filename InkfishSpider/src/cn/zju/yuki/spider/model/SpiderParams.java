81747e2834d59b00c0e965c06c84defe3c215cee
Devin <crazyacking@gmail.com>
2015-06-24 00:14:59 +0800

    start code

---
+++ InkfishSpider/src/cn/zju/yuki/spider/model/SpiderParams.java
@@ -0,0 +1,6 @@
+package cn.zju.yuki.spider.model;
+
+public class SpiderParams {
+	public static int WORKER_NUM = 1;
+	public static int DEYLAY_TIME = 1000;
+}
---
+++ InkfishSpider/src/cn/zju/yuki/spider/storage/DataStorage.java
@@ -0,0 +1,8 @@
+package cn.zju.yuki.spider.storage;
+
+public class DataStorage {
+	public void store(Object data){
+		// store to DB
+		// TODO
+	}
+}
