68c3c35cc236f6327d95a012fa0eaf88e2dd87ff
Devin <11301655@qq.com>
2015-12-16 08:43:55 +0800

    Update FriendRecommend.java

--- src/cn/edu/hut/crazyacking/spider/recommend/FriendRecommend.java
+++ src/cn/edu/hut/crazyacking/spider/recommend/FriendRecommend.java
@@ -9,5 +9,36 @@ import java.sql.Statement;
  * Created by crazyacking on 2015/7/3.
  */
 class FriendRecommend {
-  // to do
+  // to do something
+}
+
+
+
+package cn.edu.hut.crazyacking.spider.recommend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by crazyacking on 2015/7/3.
+ */
+class FriendRecommend {
+  // to do something
+}
+
+
+package cn.edu.hut.crazyacking.spider.recommend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by crazyacking on 2015/7/3.
+ */
+class FriendRecommend {
+  // to do something
 }
